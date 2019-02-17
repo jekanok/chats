aad74f79053fb15f94b61338ff59afe74e463533
Yevhenii <testjokerqwerty@gmail.com>
2019-02-17 20:54:55 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import Uimini from "uimini/dist/css/uimini.css";
+import Vuelidate from "vuelidate";
 
 Vue.config.productionTip = false
 
